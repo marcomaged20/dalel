e160698b7e673460c2b3c98c2d9a413af2f4a612
marcomaged20 <marcomaged90@gmail.com>
2023-11-27 23:23:00 +0200

    first commit

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class Appcolors {
+  static Color primarycolor = const Color(0xffC49E85);
+  static Color offwhite = const Color(0xffF8F4F9);
+  static Color deebBrown = const Color(0xff6B4B3E);
+  static Color deebGrey = const Color(0xff6F6460);
+  static Color lighGrey = const Color(0xffB4B4B4);
+  static Color black = const Color(0xff333333);
+}
